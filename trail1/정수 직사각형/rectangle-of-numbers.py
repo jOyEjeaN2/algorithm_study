5e9b4e07667f690facd1dbc5e7b9a4dcef0e6a5c
Yejin Cho <yejincho0928@gmail.com>
2026-09-06 23:02:08 +0900

    [Passed] 정수 직사각형 (52ms, 16MB)

---
+++ trail1/정수 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+
+arr = [[0] * M for _ in range(N)]
+
+num = 1
+for i in range(N):
+    for j in range(M):
+        arr[i][j] = num 
+        num += 1
+
+for row in arr:
+    print(*row)
