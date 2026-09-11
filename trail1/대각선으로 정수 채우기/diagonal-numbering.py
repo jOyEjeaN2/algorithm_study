6c98ea1ed4e5b976b4915fc395c971854d001edc
Yejin Cho <yejincho0928@gmail.com>
2026-09-11 23:32:13 +0900

    [Passed] 대각선으로 정수 채우기 (60ms, 16MB)

---
+++ trail1/대각선으로 정수 채우기/diagonal-numbering.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+arr = [[0]*m for _ in range(n)] 
+
+num = 1 
+
+for s in range(n+m-1):
+    for i in range(n):
+        j = s - i 
+        if 0 <= j < m:
+            arr[i][j] = num 
+            num += 1 
+
+for row in arr:
+    print(*row)
+        
