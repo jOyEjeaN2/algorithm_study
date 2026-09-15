54c39b112367e6d6482ab2e452a861a1940ce1a0
Yejin Cho <yejincho0928@gmail.com>
2026-09-15 21:24:19 +0900

    [Passed] 파스칼의 삼각형 (49ms, 16MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.py
@@ -0,0 +1,12 @@
+N = int(input())
+arr = [[0]* (i+1) for i in range(N)]
+
+for i in range(N):
+    for j in range(i+1):
+        if j == 0 or j == i:
+            arr[i][j] = 1
+        else:
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
+for row in arr:
+    print(*row)
