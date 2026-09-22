fcdce8fc8c7e63acbffe94c38816abd9c0dd2867
Yejin Cho <yejincho0928@gmail.com>
2026-09-22 22:13:08 +0900

    [Passed] 숫자로 이루어진 사각형 (55ms, 16MB)

--- trail2/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ trail2/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -1,19 +1,20 @@
-n = int(input())
-
 # Please write your code here.
-arr = [[0]*n for _ in range(n)]
-count = 1
+def print_number_square(n):
+    arr = [[0]*n for _ in range(n)]
+    num = 1
 
-for i in range(n):
-    for j in range(n):
-        if count <= 9:
-            arr[i][j] = count 
-            count += 1
-        else:
-            count = 1
-            arr[i][j] = count
-            count += 1
+    for i in range(n):
+        for j in range(n):
+            if num <= 9:
+                arr[i][j] = num
+                num += 1
+            else:
+                num = 1
+                arr[i][j] = num
+                num += 1
 
+    for row in arr:
+        print(*row)
 
-for row in arr:
-    print(*row)
+n = int(input())
+print_number_square(n)
